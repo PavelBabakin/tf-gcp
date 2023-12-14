91ca6e5e19c38d96256b6d1033e3bd9d109a310e
PavelBabakin <pavlobabakin@gmail.com>
2023-12-14 12:51:39 +0000

    Added initial tf.

---
+++ tf/variables.tf
@@ -0,0 +1,22 @@
+variable "GOOGLE_PROJECT" {
+  type        = string
+  description = "tf_gcp"
+}
+
+variable "GOOGLE_REGION" {
+  type        = string
+  default     = "us-central1-c"
+  description = "GCP region to use"
+}
+
+variable "GKE_MACHINE_TYPE" {
+  type        = string
+  default     = "g1-small"
+  description = "Machine type"
+}
+
+variable "GKE_NUM_NODES" {
+  type        = number
+  default     = 5
+  description = "GKE nodes number"
+}
