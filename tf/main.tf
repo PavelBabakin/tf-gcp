19f5aaef6c17d29acfc588846db1986c5c5d2eaa
PavelBabakin <pavlobabakin@gmail.com>
2023-12-14 21:40:16 +0000

    Added initial infra.

--- tf/main.tf
+++ tf/main.tf
@@ -1,6 +1,13 @@
 module "gke_cluster" {
-  source         = "https://github.com/PavelBabakin/tf-gcp"
+  source         = "github.com/PavelBabakin/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "tf_gcp"
+    prefix = "terraform/state"
+  }
 }
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,18 +1,12 @@
 variable "GOOGLE_PROJECT" {
   type        = string
-  description = "tf_gcp"
+  description = "GCP project id"
 }
 
 variable "GOOGLE_REGION" {
   type        = string
   default     = "us-central1-c"
   description = "GCP region to use"
-}
-
-variable "GKE_MACHINE_TYPE" {
-  type        = string
-  default     = "g1-small"
-  description = "Machine type"
 }
 
 variable "GKE_NUM_NODES" {
